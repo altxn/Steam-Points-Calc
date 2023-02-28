90f95f2ad5c0a425965554fc59bbc0734c08e17d
altxn <altkuq@gmail.com>
2023-02-28 11:14:17 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Steam-Points-Calc
+# Steam Points Calculator
+
+Calculate how many Steam Points you earn by spending X amount of money:
---
+++ script.js
@@ -0,0 +1,29 @@
+function calculate() {
+    const moneyToConvert = parseFloat($("#amount").val());
+    const currency = $("#currency").val();
+
+    let pointsToCurrency;
+    switch (currency) {
+        case "USD":
+            // 100 points for every 1 dollar you spend
+            pointsToCurrency = 100;
+            break;
+        case "EUR":
+            // 118 points for every 1 eur you spend
+            pointsToCurrency = 118;
+            break;
+        case "NOK":
+            // 100 points for every 10,00 kr you spend
+            pointsToCurrency = 100 / 10;
+            break;
+        default:
+            alert("Select currency from the dropdown list.");
+            return;
+    }
+    if (isNaN(moneyToConvert)) {
+        alert("Enter a valid number.");
+    } else if (moneyToConvert >= 0) {
+        const points = pointsToCurrency * moneyToConvert;
+        $("#amountinfo").text("You get " + points + " Steam Points");
+    }
+}
